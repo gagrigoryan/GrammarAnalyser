f27e1c9156a5b0672761591be4f1b1fc6fb14784
Gor Grigoryan <grigoryang085@gmail.com>
2020-06-28 15:45:34 +0300

    Release.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ using namespace std;
 int main() {
     Grammar g = Grammar();
     g.loadGrammar(ifstream("D:/univer/ProgUniver/Grammar/grammar.txt"));
+    g.printFIRST(cout);
+    cout << endl << endl;
+    g.printFOLLOW(cout);
+    cout << endl << endl;
+    g.printSATable();
+
+    cout << g.parse("id * id + id");
 
     return 0;
 }
